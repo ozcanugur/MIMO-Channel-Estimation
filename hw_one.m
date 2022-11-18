f62b334f12f64fbaab3f44446567e73eda327c4c
Ugur Ozcan <ozcanyi@itu.edu.tr>
2022-11-18 20:08:12 +0100

    w/ Kagan

--- hw_one.m
+++ hw_one.m
@@ -6,14 +6,15 @@ clc
 
 %% 1.1 Noise Generation
 
-N = 2;
+N = 4;
 L = round(linspace(10,200,10));
 rho = linspace(0,0.99,10);
 
+% rhoyu düzelt
 for i = 1 : length(rho)
     for j = 1 : length(L)
         for mc = 1 : 500
-            Cw = [1, rho(i); rho(i) 1];
+            Cw = toeplitz([1, rho(i)*ones(1,N-1)]);
             w = sqrtm(Cw) * randn(N, L(j));
             Cw_est = (w * w') / L(j);
             errC = (Cw - Cw_est).^2;
@@ -50,15 +51,32 @@ training_bits = bits(1:Q);
 
 SNR = db2pow(SNRdb);
 
+Cw = toeplitz([1, 0*ones(1,M-1)]);
+W= sqrtm(Cw) * randn(M, Q);
 
 for i=1:M
     for j=1:M
         for n=1:N
-            H{i,j,n} = alpha(n)^(abs(i-j))*beta;
+            H(i,j) = alpha(n)^(abs(i-j))*beta;
         end
     end
 end
+%%
 
+Q = linspace(1, 50, N);
 
-H_est = ones(M,M)
+for q = Q
+    for it = 100
+        X = randi(2,M,Q)-1;
+        h = H(:);
+        w = reshape(W',[numel(W) 1]);
+        X_bp = kron(eye(4),X');
+        
+        y = X_bp*h + w;
+        
+        h_hat_mle = (X_bp'*(Cw\X_bp))\(X_bp'*(Cw\y));
+        err_mle(Q,it) = mean((h_hat_mle - h).^2);
+    end
+end
 
+err = mean(err_mle);
