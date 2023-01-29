07593bee65cdafee8dce360a74120c0b7553f852
Ugur Ozcan <ugurzcan@gmail.com>
2023-01-29 21:05:02 +0100

    noise ok

--- hw_one.m
+++ hw_one.m
@@ -7,14 +7,18 @@ clc
 %% 1.1 Noise Generation
 
 N = 4;
+M = 4;
 L = round(linspace(10,200,10));
 rho = linspace(0,0.99,10);
 
-% rhoyu düzelt
 for i = 1 : length(rho)
     for j = 1 : length(L)
+        for input=1:M
+            for output=1:N
+                Cw(output,input)=rho(i)^(abs(output-input));
+            end
+        end
         for mc = 1 : 500
-            Cw = toeplitz([1, rho(i)*ones(1,N-1)]);
             w = sqrtm(Cw) * randn(N, L(j));
             Cw_est = (w * w') / L(j);
             errC = (Cw - Cw_est).^2;
@@ -24,64 +28,44 @@ for i = 1 : length(rho)
 end
 
 figure; hold on
-plot(L, mean(mse,3).', 'LineWidth', 2)
+plot(L, mean(mse(1,:,:),3).', 'LineWidth', 2, "DisplayName",strcat("\rho=",string(rho(1))))
+plot(L, mean(mse(5,:,:),3).', 'LineWidth', 2, "DisplayName",strcat("\rho=",string(rho(5))))
+plot(L, mean(mse(10,:,:),3).', 'LineWidth', 2, "DisplayName",strcat("\rho=",string(rho(10))))
 xlabel('Number of samples [L]');
 ylabel('MSE');
-
+grid
+legend
 
 figure; hold on
-plot(rho, mean(mse(:,[1, 10],:),3), 'LineWidth', 2)
+plot(rho, mean(mse(:,1,:),3), 'LineWidth', 2, "DisplayName",strcat("L=",string(L(1))))
+plot(rho, mean(mse(:,5,:),3), 'LineWidth', 2, "DisplayName",strcat("L=",string(L(5))))
+plot(rho, mean(mse(:,10,:),3), 'LineWidth', 2, "DisplayName",strcat("L=",string(L(10))))
 xlabel('Correlation coefficient [\rho]');
 ylabel('MSE');
+grid
+legend
 
-% %% 
-% clear
-% 
-% 
-% N = 5;
-% SNRdb = -10:2:30;
-% %Q = linspace(1, 50, N);
-% Q = 10;
-% alpha = linspace(0, .99, N);
-% beta = 0.9;
-% M = 4;
-% 
-% bits = randi(2,1,1e3)-1;
-% training_bits = bits(1:Q);
-% 
-% SNR = db2pow(SNRdb);
-% 
-% Cw = toeplitz([1, 0*ones(1,M-1)]);
-% W= sqrtm(Cw) * randn(M, Q);
-% 
-% for i=1:M
-%     for j=1:M
-%         for n=1:N
-%             H(i,j) = alpha(n)^(abs(i-j))*beta;
-%         end
-%     end
-% end
 %%
-% 
+%
 % Q = linspace(10, 50, N);
-% 
+%
 % for q = Q
 %     for it = 100
 %         X = randi(2,M,q)-1;
 %         h = H(:);
 %         w = reshape(W',[numel(W) 1]);
 %         X_bp = kron(eye(4),X');
-%         
+%
 %         y = X_bp*h + w;
-%         
+%
 %         h_hat_mle = (X_bp'*(Cw\X_bp))\(X_bp'*(Cw\y));
 %         err_mle(Q,it) = mean((h_hat_mle - h).^2);
 %     end
 % end
-% 
+%
 % err = mean(err_mle);
 
-%% 
+%%
 clear
 clc
 
@@ -110,8 +94,8 @@ end
 
 
 
-for input=1:M       
-    for output=1:M  
+for input=1:M
+    for output=1:M
         C(output,input)=rho^(abs(output-input));
     end
 end
@@ -127,22 +111,22 @@ for s = 1:length(SNR)
     for a = 1:length(alpha)
         for q = 1:length(Q)
             for it = 1:10
-    %           X = 2*randi([0,1],M,Q(q))-1;
+                %           X = 2*randi([0,1],M,Q(q))-1;
                 X = sqrt(SNR(s)).*randn(M,Q(q));
-    %           X = sqrt(SNR(s)).*X;
+                %           X = sqrt(SNR(s)).*X;
                 h = H{a}(:);
                 W = sqrtm(C) * randn(M, Q(q));
                 w = reshape(W',[numel(W) 1]);
                 X_bp = kron(eye(4),X');
-                
+
                 y = X_bp*h + w;
-                
+
                 h_hat_mle = (X_bp'*(CW{1,q}\X_bp))\(X_bp'*(CW{1,q}\y));
                 err_mle(q,s,a,it) = nanmean((h_hat_mle - h).^2);
-    
-    %           crb_mat{s,q} =  inv(X_bp'*inv(CW{1,q})*X_bp);
+
+                %           crb_mat{s,q} =  inv(X_bp'*inv(CW{1,q})*X_bp);
                 crb_mat{q,s,a} =  inv(X_bp'*X_bp);
-    
+
                 CRB(q,s,a,it) = trace(crb_mat{q,s,a})/length(crb_mat{q,s,a});
             end
         end
@@ -170,7 +154,7 @@ hold on
 % loglog(SNR,CRBa,"DisplayName","CRBa")
 
 
-%% deneme3 
+%% deneme3
 
 N = size(X',1);
 M = size(X',2);
@@ -184,7 +168,7 @@ end
 
 %%
 clear
-clc 
+clc
 
 
 
@@ -211,8 +195,8 @@ for i=1:M
 end
 
 
-for input=1:M       
-    for output=1:M  
+for input=1:M
+    for output=1:M
         C(output,input)=rho^(abs(output-input));
     end
 end
@@ -222,7 +206,7 @@ end
 %     for q = 1:length(Q)
 %         CW{r,q} = toeplitz([1.*ones(1,K), zeros(1,Q(q)-1), rho(r).*ones(1,K), zeros(1,Q(q)-1), rho(r)^2.*ones(1,K), zeros(1,Q(q)-1), rho(r)^3.*ones(1,K), zeros(1,Q(q)-1)]);
 %     end
-% end 
+% end
 for r = 1:length(rho)
     for q = 1:length(Q)
         CW{r,q} = toeplitz([1, zeros(1,Q(q)-2+K), rho(r), zeros(1,Q(q)-2+K), rho(r)^2, zeros(1,Q(q)-2+K), rho(r)^3, zeros(1,Q(q)-2+K)]);
@@ -249,7 +233,7 @@ for s = 1:length(SNR)
 
                 X_bp = kron(eye(4),X_conv);
                 y = X_bp*h + w;
-                
+
                 h_hat_mle = (X_bp'*(CW{1,q}\X_bp))\(X_bp'*(CW{1,q}\y));
                 err_mle(s,a,q,it) = mean((h_hat_mle - h).^2);
 
@@ -272,7 +256,7 @@ hold on
 % loglog(SNR,CRB2,"DisplayName","CRB")
 
 
-%% 
+%% MIMO DEC.
 clear
 clc
 
@@ -303,8 +287,8 @@ end
 
 
 
-for input=1:M       
-    for output=1:M  
+for input=1:M
+    for output=1:M
         C(output,input)=rho^(abs(output-input));
     end
 end
@@ -328,7 +312,7 @@ for s = 1:length(SNR)
             wall = reshape(Wall',[numel(Wall) 1]);
             X_bp = kron(eye(4),X');
             X_bpall = kron(eye(M),Xall');
-            
+
             y = X_bp*h + w;
 
             h_mle = (X_bp'*(CW\X_bp))\(X_bp'*(CW\y));
@@ -345,7 +329,7 @@ for s = 1:length(SNR)
             x_mle = (HHall'*(CW2\HHall))\(HHall'*(CW2\y_all2));
             err_mle(q,s,it) = mean(mean((x_mle - xall).^2));
             CC=sqrt(SNR(s))*eye(size(HHall));
-%             x_mmse = CC*HHall'*inv(HHall*CC*HHall'+CW2)*y_all2;
+            %             x_mmse = CC*HHall'*inv(HHall*CC*HHall'+CW2)*y_all2;
             x_mmse = inv(HHall'*inv(CW2)*HHall+inv(CC))*HHall'*(CW2\y_all2);
             err_mmse(q,s,it) = mean(mean((x_mmse - xall).^2));
             x_dn = inv(HHdn'*inv(CW2)*HHdn)*(HHdn'*(CW2\y_all2));
